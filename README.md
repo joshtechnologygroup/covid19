5cffa776e650e38af53d5d7f173ee87bacd84859
Mintu Sah <mintu@jtgs-MacBook-Pro.local>
2020-05-17 18:43:09 +0530

    BE Base Setup

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # covid19_platform
 Covid-19 Backend
+
+
+# Development Setup
+- Create virtual environment
+    `python3 -m venv .venv`
+
+- Activate virtual environment
+    `source .venv/bin/activate`
+
+- Install development requirement
+    `pip install -r requirements.dev.txt `
+
+- Create Database
+    `CREATE DATABASE covid19 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci`
+
+- Setup Database Table
+    `python manage.py migrate`
+
+- Run Local development server
+    `python manage.py runserver`
+
+- Access Django Admin at http://localhost:8000/admin/  
---
+++ manage.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+from covid19.libs.utils import get_default_django_settings_module
+
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", get_default_django_settings_module())
+    os.environ.setdefault('DJANGO_CONFIGURATION', 'Settings')
+    # NOTE: instead of changing the DJANGO_CONFIGURATION
+    # class for each environment we are change the DJANGO_SETTINGS_MODULE
+    # and keeping the same class (i.e. Settings) for all environments.
+
+    from configurations.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
